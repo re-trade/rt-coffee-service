89d6293cea46b1e45ba8312513725702f4b8894a
AnataAria <arisavinh2003@gmail.com>
2025-05-11 14:23:49 +0700

    deploy: add basic k8s and remove db for auth

--- services/authentication/src/main/java/org/vietnamsea/authentication/controller/AuthenticationController.java
+++ services/authentication/src/main/java/org/vietnamsea/authentication/controller/AuthenticationController.java
@@ -4,21 +4,26 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.vietnamsea.authentication.model.dto.request.AuthenticationRequest;
 import org.vietnamsea.authentication.model.dto.request.ExternalCustomerAccountAuthRequest;
 import org.vietnamsea.authentication.model.dto.response.AuthResponse;
 import org.vietnamsea.authentication.service.AuthService;
 import org.vietnamsea.common.model.dto.response.ResponseObject;
 
+import java.awt.image.BufferedImage;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(path = "auth")
 public class AuthenticationController {
     private final AuthService authService;
+
+    @PostMapping("register/2fa")
+    public ResponseEntity<BufferedImage> signupTwoFactorAuth (@RequestParam(name = "width", value = "300") int width,@RequestParam(name = "height", value = "300") int height) {
+        var result = authService.register2FaAuthentication(width, height);
+        return ResponseEntity.ok(result);
+    }
 
     @PostMapping("local")
     public ResponseEntity<ResponseObject<AuthResponse>> customerAuthenticationInternal(@Valid @RequestBody AuthenticationRequest request, HttpServletResponse response) {
