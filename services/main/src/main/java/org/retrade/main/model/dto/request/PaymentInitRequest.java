1482bd2921822c11d51a0b2e80fc8fb03c3e8e45
AnataAria <arisavinh2003@gmail.com>
2025-06-08 02:24:29 +0700

    misc: update deploy

--- services/main/src/main/java/org/retrade/main/model/dto/request/PaymentInitRequest.java
+++ services/main/src/main/java/org/retrade/main/model/dto/request/PaymentInitRequest.java
@@ -1,11 +1,15 @@
 package org.retrade.main.model.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PaymentInitRequest {
 
     @NotBlank(message = "Payment method ID is required")
--- services/main/src/main/java/org/retrade/main/model/dto/response/CustomerContactResponse.java
+++ services/main/src/main/java/org/retrade/main/model/dto/response/CustomerContactResponse.java
@@ -2,9 +2,11 @@ package org.retrade.main.model.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Builder
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class CustomerContactResponse {
