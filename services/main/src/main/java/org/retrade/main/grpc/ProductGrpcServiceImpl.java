a9e3dbe925e19dc6ba0e565eb882232ccc3b9fdf
AnataAria <arisavinh2003@gmail.com>
2025-06-12 21:07:33 +0700

    misc: update discount from string to double

--- services/main/src/main/java/org/retrade/main/grpc/ProductGrpcServiceImpl.java
+++ services/main/src/main/java/org/retrade/main/grpc/ProductGrpcServiceImpl.java
@@ -224,7 +224,7 @@ public class ProductGrpcServiceImpl extends GrpcProductServiceGrpc.GrpcProductSe
                 .setDescription(product.getDescription() != null ? product.getDescription() : "")
                 .setThumbnail(product.getThumbnail() != null ? product.getThumbnail() : "")
                 .setBrand(product.getBrand())
-                .setDiscount(product.getDiscount() != null ? product.getDiscount() : "")
+                .setDiscount(product.getDiscount() != null ? product.getDiscount() : 0)
                 .setModel(product.getModel())
                 .setCurrentPrice(product.getCurrentPrice().doubleValue())
                 .setVerified(product.getVerified() != null ? product.getVerified() : false);
