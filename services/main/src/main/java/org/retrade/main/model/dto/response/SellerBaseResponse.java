0bc60ffd294cc4be78cfe5b985bf9c325782366b
AnataAria <arisavinh2003@gmail.com>
2025-06-14 05:26:57 +0700

    misc: update date response error

--- services/main/src/main/java/org/retrade/main/model/dto/response/SellerBaseResponse.java
+++ services/main/src/main/java/org/retrade/main/model/dto/response/SellerBaseResponse.java
@@ -1,5 +1,6 @@
 package org.retrade.main.model.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -22,6 +23,8 @@ public class SellerBaseResponse {
     private String background;
     private String phoneNumber;
     private Boolean verified;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createdAt;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime updatedAt;
 }
