7c8ed9c8896f5e22545900b902f6a62873c1aec1
haiha0402 <nguyenha24bmt@gmail.com>
2025-06-23 14:39:34 +0700

    fix merge

--- services/main/src/main/java/org/retrade/main/repository/AccountRoleRepository.java
+++ services/main/src/main/java/org/retrade/main/repository/AccountRoleRepository.java
@@ -2,14 +2,8 @@ package org.retrade.main.repository;
 
 import org.retrade.common.repository.BaseJpaRepository;
 import org.retrade.main.model.entity.AccountRoleEntity;
-<<<<<<< HEAD
-
-public interface AccountRoleRepository extends BaseJpaRepository<AccountRoleEntity, String> {
-}
-=======
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AccountRoleRepository extends BaseJpaRepository<AccountRoleEntity, String> {
 }
->>>>>>> 644b29bb29325c5f33ef47e964a21213396462ca
