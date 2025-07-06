d9754fdcedd0e873c49519b0a9fbe64c2bf2d668
tohka-vo-tri <imchibitouka@gmail.com>
2025-07-06 21:59:14 +0700

    add filed list category response

---
+++ services/main/src/main/java/org/retrade/main/model/dto/response/CategoryBaseResponse.java
@@ -0,0 +1,15 @@
+package org.retrade.main.model.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CategoryBaseResponse {
+    private String id;
+    private String name;
+}
