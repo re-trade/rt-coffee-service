5fff39b5e4804f21de58f4fc9021012452efebf1
AnataAria <arisavinh2003@gmail.com>
2025-08-05 09:14:14 +0700

    feat: add date formatting to `WithdrawRequestBaseResponse` DTO
    - Applied `@JsonFormat` annotation to `createdDate` and `processedDate` fields for consistent date formatting (`yyyy-MM-dd HH:mm:ss`).

--- services/main/src/main/java/org/retrade/main/model/dto/response/WithdrawRequestBaseResponse.java
+++ services/main/src/main/java/org/retrade/main/model/dto/response/WithdrawRequestBaseResponse.java
@@ -1,5 +1,6 @@
 package org.retrade.main.model.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,7 +18,9 @@ public class WithdrawRequestBaseResponse {
     private String id;
     private BigDecimal amount;
     private WithdrawStatusEnum status;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createdDate;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime processedDate;
     private String bankBin;
     private String bankName;
