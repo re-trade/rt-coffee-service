2125c7c5969c69e4b1dd7023ad6b1c171bd9a739
AnataAria <arisavinh2003@gmail.com>
2025-08-10 03:42:41 +0700

    refactor: remove unused `CHAT` notification type constant from `NotificationTypeCode`

--- services/feedback-notification/src/main/java/org/retrade/feedback_notification/model/constant/NotificationTypeCode.java
+++ services/feedback-notification/src/main/java/org/retrade/feedback_notification/model/constant/NotificationTypeCode.java
@@ -2,7 +2,6 @@ package org.retrade.feedback_notification.model.constant;
 
 public class NotificationTypeCode {
     public static final String SYSTEM = "SYSTEM";
-    public static final String CHAT = "CHAT";
     public static final String ORDER = "ORDER";
     public static final String ALERT = "ALERT";
 }
