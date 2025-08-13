63a09bd87683f476a0a8c95bb75d2ef7bc1b21ae
NGUYỄN HẢI HÀ <75192598+haiha0402@users.noreply.github.com>
2025-08-13 19:28:32 +0700

    fix enable, disable customer

--- services/main/src/main/java/org/retrade/main/repository/jpa/AccountRoleRepository.java
+++ services/main/src/main/java/org/retrade/main/repository/jpa/AccountRoleRepository.java
@@ -3,7 +3,14 @@ package org.retrade.main.repository.jpa;
 import org.retrade.common.repository.BaseJpaRepository;
 import org.retrade.main.model.entity.AccountRoleEntity;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
 
 @Repository
 public interface AccountRoleRepository extends BaseJpaRepository<AccountRoleEntity, String> {
+    @Query("SELECT ar FROM account_roles ar WHERE ar.account.id = :accountId AND ar.role.id = :roleId")
+    Optional<AccountRoleEntity> findByAccountIdAndRoleId(@Param("accountId") String accountId, @Param("roleId") String roleId);
 }
